dae23c8bfe336499900d3216fecf6479de920756
isus-ipanienko <pawel.rel@gmail.com>
2021-02-20 12:02:14 +0100

    changed layout, changed curve_fit to linear regression

--- MatPlotScript/MatPlotScript.py
+++ MatPlotScript/MatPlotScript.py
@@ -11,12 +11,7 @@ import matplotlib.pyplot as plt
 import csv
 import numpy as np 
 import math as m 
-from scipy.optimize import curve_fit 
-
-
-### fitting function
-def scifunc(x, A, offset):
-    return A * np.sin(2*x/57.2957795) + offset 
+from scipy.stats import linregress 
 
 
 class Window(QDialog):
@@ -28,6 +23,21 @@ class Window(QDialog):
         self.col_count = None
         self.data = [[],[]]
         self.y_fit = []
+
+
+        ### options setup
+        # t_options
+        self.t_options = QTableWidget()
+        self.t_options.setRowCount(1)
+        self.t_options.setColumnCount(5)
+        self.t_options.setVerticalHeaderLabels(['Options'])
+        self.t_options.resizeColumnsToContents()
+        self.t_options.setItem(0,0, QTableWidgetItem('axis (x/y)'))
+        self.t_options.setItem(0,1, QTableWidgetItem('domain'))
+        self.t_options.setItem(0,2, QTableWidgetItem('colour'))
+        self.t_options.setItem(0,3, QTableWidgetItem('pattern'))
+        self.t_options.setItem(0,4, QTableWidgetItem('markersize'))
+
 
         ### user input setup
         # buttons
@@ -48,64 +58,57 @@ class Window(QDialog):
         self.e_ylabel = QLineEdit('ylabel')
         # input_box
         self.input_box = QVBoxLayout()
-        self.input_box.addWidget(self.b_plot)
-        self.input_box.addWidget(self.b_read)
-        self.input_box.addWidget(self.l_path)
-        self.input_box.addWidget(self.e_path)
-        self.input_box.addWidget(self.l_delimiter)
-        self.input_box.addWidget(self.e_delimiter)
-        self.input_box.addWidget(self.l_title)
-        self.input_box.addWidget(self.e_title)
-        self.input_box.addWidget(self.l_xlabel)
-        self.input_box.addWidget(self.e_xlabel)
-        self.input_box.addWidget(self.l_ylabel)
-        self.input_box.addWidget(self.e_ylabel)
+        self.input_box.addWidget(self.t_options)
+        self.top_input_box = QHBoxLayout()
+        self.input_box.addLayout(self.top_input_box)
+        self.left_input_box = QVBoxLayout()
+        self.right_input_box = QVBoxLayout()
+        self.top_input_box.addLayout(self.left_input_box)
+        self.top_input_box.addLayout(self.right_input_box)
+        self.left_input_box.addWidget(self.b_plot)
+        self.right_input_box.addWidget(self.b_read)
+        self.left_input_box.addWidget(self.l_path)
+        self.right_input_box.addWidget(self.e_path)
+        self.left_input_box.addWidget(self.l_delimiter)
+        self.right_input_box.addWidget(self.e_delimiter)
+        self.left_input_box.addWidget(self.l_title)
+        self.right_input_box.addWidget(self.e_title)
+        self.left_input_box.addWidget(self.l_xlabel)
+        self.right_input_box.addWidget(self.e_xlabel)
+        self.left_input_box.addWidget(self.l_ylabel)
+        self.right_input_box.addWidget(self.e_ylabel)
         
 
         ### curve_fit setup
         # buttons
-        self.b_curve_plot = QPushButton('Plot with start params')
-        self.b_curve_plot.clicked.connect(lambda: self.plot('plot_curve'))
+        self.l_curve_calc = QLabel('Linear regression:')
         self.b_curve_calc = QPushButton('Calculate fit')
         self.b_curve_calc.clicked.connect(lambda: self.plot('calc_curve'))
         # labels
-        self.l_expression = QLabel('Expression params:')
-        self.e_expression = QLineEdit('3, 5')
+        self.l_expression = QLabel('Significant numbers:')
+        self.e_expression = QLineEdit('3')
         self.l_x_curve = QLabel('x-axis (domain):')
         self.e_x_curve = QLineEdit('x1')
         self.l_y_curve = QLabel('y-axis (label):')
         self.e_y_curve = QLineEdit('y1')
-        self.l_params = QLabel('Parameters:')
+        self.l_params = QLabel('Fit params:')
         self.e_params = QLineEdit('')
         # input_box
-        self.input_box.addWidget(self.b_curve_plot)
-        self.input_box.addWidget(self.b_curve_calc)
-        self.input_box.addWidget(self.l_expression)
-        self.input_box.addWidget(self.e_expression)
-        self.input_box.addWidget(self.l_x_curve)
-        self.input_box.addWidget(self.e_x_curve)
-        self.input_box.addWidget(self.l_y_curve)
-        self.input_box.addWidget(self.e_y_curve)
-        self.input_box.addWidget(self.l_params)
-        self.input_box.addWidget(self.e_params)
+        self.left_input_box.addWidget(self.l_curve_calc)
+        self.right_input_box.addWidget(self.b_curve_calc)
+        self.left_input_box.addWidget(self.l_expression)
+        self.right_input_box.addWidget(self.e_expression)
+        self.left_input_box.addWidget(self.l_x_curve)
+        self.right_input_box.addWidget(self.e_x_curve)
+        self.left_input_box.addWidget(self.l_y_curve)
+        self.right_input_box.addWidget(self.e_y_curve)
+        self.left_input_box.addWidget(self.l_params)
+        self.right_input_box.addWidget(self.e_params)
 
 
         ### error_label
         self.l_error = QLabel('Error: None')
         self.input_box.addWidget(self.l_error)
-
-
-        ### options setup
-        # t_options
-        self.t_options = QTableWidget()
-        self.t_options.setRowCount(1)
-        self.t_options.setColumnCount(5)
-        self.t_options.setVerticalHeaderLabels(['Options'])
-        self.t_options.setItem(0,0, QTableWidgetItem('axis (x/y)'))
-        self.t_options.setItem(0,1, QTableWidgetItem('domain'))
-        self.t_options.setItem(0,2, QTableWidgetItem('colour'))
-        self.t_options.setItem(0,3, QTableWidgetItem('pattern'))
-        self.t_options.setItem(0,4, QTableWidgetItem('markersize'))
 
 
         ### matplot setup
@@ -130,7 +133,6 @@ class Window(QDialog):
         # layout
         self.layout = QHBoxLayout()
         self.layout.addLayout(self.input_box)
-        self.layout.addWidget(self.t_options)
         self.layout.addLayout(self.matplot_box)
         self.layout.addWidget(self.t_spread)
         self.setLayout(self.layout)
@@ -235,41 +237,42 @@ class Window(QDialog):
                 handle_tab.append(mpatches.Patch(color=self.t_options.item(col+1,2).text(), label=self.data[0][col]))
         
         # plot user fit
-        if curve == 'plot_curve':
-            try:
-                temp_domain_index = domain_list.index(self.e_x_curve.text()) # get index of x domain
-                temp_y_index = self.data[0].index(self.e_y_curve.text()) # get index of y
-            except:
-                    self.l_error.setText('missing domain')
-                    return
+        #if curve == 'plot_curve':
+        #    try:
+        #        temp_domain_index = domain_list.index(self.e_x_curve.text()) # get index of x domain
+        #        temp_y_index = self.data[0].index(self.e_y_curve.text()) # get index of y
+        #    except:
+        #            self.l_error.setText('missing domain')
+        #            return
             
-            try:
-                temp_x = []
-                for row in range(self.row_count):
-                    temp_x.append(float(self.data[row+1][temp_domain_index]))
-                temp_x = np.array(temp_x)
-            except:
-                    self.l_error.setText('chart ValueError')
-                    return
+        #    try:
+        #        temp_x = []
+        #        for row in range(self.row_count):
+        #            temp_x.append(float(self.data[row+1][temp_domain_index]))
+        #        temp_x = np.array(temp_x)
+        #    except:
+        #            self.l_error.setText('chart ValueError')
+        #            return
 
-            try:
-                temp_params = self.e_expression.text().split(', ') # user input
-                temp_params = [int(k) for k in temp_params]
-            except:
-                    self.l_error.setText('params ValueError')
-                    return
-            try:
-                temp_y = scifunc(temp_x, *temp_params)
-            except:
-                    self.l_error.setText('wrong params')
-                    return
+        #    try:
+        #        temp_params = self.e_expression.text().split(', ') # user input
+        #        temp_params = [float(k) for k in temp_params]
+        #    except:
+        #            self.l_error.setText('params ValueError')
+        #            return
+        #    try:
+        #        temp_y = []
+        #        temp_y = temp_params[0] * temp_x + temp_params[1]
+        #    except:
+        #            self.l_error.setText('wrong params')
+        #            return
             
-            #                            colour                                 pattern                         markersize
-            ax.plot(temp_x, temp_y, self.t_options.item(temp_y_index+1,2).text() + '-', ms = int(self.t_options.item(temp_y_index+1,4).text()))
-            #                                                  colour                              label
-            handle_tab.append(mpatches.Patch(color=self.t_options.item(temp_y_index+1,2).text(), label=self.data[0][temp_y_index] + ' fit test'))
+        #    #                            colour                                 pattern                         markersize
+        #    ax.plot(temp_x, temp_y, self.t_options.item(temp_y_index+1,2).text() + '-', ms = int(self.t_options.item(temp_y_index+1,4).text()))
+        #    #                                                  colour                              label
+        #    handle_tab.append(mpatches.Patch(color=self.t_options.item(temp_y_index+1,2).text(), label=self.data[0][temp_y_index] + ' fit test'))
         
-        # plot curve fit
+        # plot linear regression
         if curve == 'calc_curve':
             try:
                 temp_domain_index = domain_list.index(self.e_x_curve.text()) # get index of x domain
@@ -291,18 +294,21 @@ class Window(QDialog):
                     return
         
             try:
-                fit_params, covariance_matrix = curve_fit(scifunc, temp_y, temp_x)
+                slope, intercept, r_value, p_value, std_err = linregress(temp_x, temp_y)
+                significant_numbers = int(self.e_expression.text())
+                rounded_slope =  round(slope, significant_numbers - int(m.floor(m.log10(abs(slope)))) - 1)
+                rounded_intercept =  round(intercept, significant_numbers - int(m.floor(m.log10(abs(intercept)))) - 1)
             except:
-                    self.l_error.setText('mismatched domain')
+                    self.l_error.setText('mismatched domain or wrong significant numbers')
                     return
         
             #                            colour                                 pattern                         markersize
-            ax.plot(temp_x, scifunc(temp_x, *fit_params), self.t_options.item(temp_y_index+1,2).text() + '-', ms = int(self.t_options.item(temp_y_index+1,4).text()))
+            ax.plot(temp_x, slope * temp_x + intercept, self.t_options.item(temp_y_index+1,2).text() + '-', ms = int(self.t_options.item(temp_y_index+1,4).text()))
             #                                                  colour                              label
-            handle_tab.append(mpatches.Patch(color=self.t_options.item(temp_y_index+1,2).text(), label=self.data[0][temp_y_index] + ' fit'))
+            handle_tab.append(mpatches.Patch(color=self.t_options.item(temp_y_index+1,2).text(), label=f'y = {rounded_slope} * x + {rounded_intercept}'))
 
             # output parameters
-            self.e_params.setText(str(fit_params))
+            self.e_params.setText(f'y = {slope} * x + {intercept} R^2: {r_value**2}, P: {p_value}, STD: {std_err}')
 
         # draw legend
         try:
@@ -331,6 +337,8 @@ class Window(QDialog):
 
     def dropEvent(self, e):
         self.e_path.setText(e.mimeData().text()[8:])
+        self.read()
+        self.plot('no curve')
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
